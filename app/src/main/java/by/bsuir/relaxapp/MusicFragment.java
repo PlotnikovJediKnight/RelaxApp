21ec7467be304c34eb65e132a0f5bd41a86e6b30
Vladislav Plotnikov <vadimovich74@gmail.com>
2022-04-11 05:09:24 +0300

    Eleventh commit

--- app/src/main/java/by/bsuir/relaxapp/MusicFragment.java
+++ app/src/main/java/by/bsuir/relaxapp/MusicFragment.java
@@ -1,10 +1,12 @@
 package by.bsuir.relaxapp;
 
+import com.google.type.DateTime;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
 import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.Image;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
@@ -17,32 +19,124 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MusicFragment extends Fragment {
 
+    private boolean gonna_play = false;
+    private static final String CLIENT_ID = "8cd0b669498e49d8b1915d6da500e789";
+    private static final String REDIRECT_URI = "https://localhost:8080";
+    public static SpotifyAppRemote mSpotifyAppRemote = null;
+
+    public static final String SUPP_PLAYLIST_PLAY_STR = "spotify:playlist:";
+    public static final String PLAYLIST_IDS[] = new String[]
+            {   "16b887lG3wgyYtXus4PfFT",
+                "6UZaOdd7TOy33Xhl3NcJFn",
+                "0baxQIQd1HKmmRj4IBAaYf",
+                "4jPiN7G9ukHxm9Zzoi4Fqp",
+                "7fQVIt29rxa92ZlmNCnlXB",
+                "3iGrDRstfZj4uCHdnpWgNp"};
+
+    public static final String PLAYLIST_NAMES[] = new String[]
+            {       "Nirvana",
+                    "GrOb",
+                    "Gorillaz",
+                    "Michael Jackson",
+                    "Modern Talking",
+                    "Avril Lavigne"};
+
+    private TextView
+            playlistName,
+            albumName,
+            artistName,
+            songName;
+
+    private ImageView
+            coverImage,
+            previousSong,
+            playStopSong,
+            nextSong;
+
+    private void findAllTextViews(View view){
+        playlistName = view.findViewById(R.id.playlistName);
+        albumName = view.findViewById(R.id.albumName);
+        artistName = view.findViewById(R.id.artistName);
+        songName = view.findViewById(R.id.songName);
+    }
+
+    private void findAllImageViews(View view){
+        coverImage = view.findViewById(R.id.coverImage);
+        previousSong = view.findViewById(R.id.previousSong);
+        playStopSong = view.findViewById(R.id.playStopSong);
+        nextSong = view.findViewById(R.id.nextSong);
+    }
+
+    private int getPlayListIndexToPlay(){
+        int max = MainActivity.APP_MOODS[0].clickedCount, ind = 0;
+
+        for (int i = 1; i < MainActivity.MOODS_COUNT; ++i){
+            if (MainActivity.APP_MOODS[i].clickedCount > max){
+                max = MainActivity.APP_MOODS[i].clickedCount;
+                ind = i;
+            }
+        }
+
+        Date date = new Date();
+        return (date.getHours() + date.getMinutes()  + ind) % MainActivity.MOODS_COUNT;
+    }
+
+    private int current_index = 0;
+
     private void connected(){
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        current_index = getPlayListIndexToPlay();
+        mSpotifyAppRemote.getPlayerApi().play(SUPP_PLAYLIST_PLAY_STR + PLAYLIST_IDS[current_index]);
 
         mSpotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
                 .setEventCallback(playerState -> {
                     final Track track = playerState.track;
                     if (track != null) {
-                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                        artistName.setText(track.artist.name);
+                        songName.setText(track.name);
+                        albumName.setText(track.album.name);
+                        playlistName.setText(PLAYLIST_NAMES[current_index]);
+                        mSpotifyAppRemote
+                                .getImagesApi()
+                                .getImage(playerState.track.imageUri, Image.Dimension.LARGE)
+                                .setResultCallback(
+                                        bitmap -> {
+                                            coverImage.setImageBitmap(bitmap);
+                                        });
                     }
                 });
+    }
+
+    public static void previousMusic(){
+        if (mSpotifyAppRemote != null) {
+            mSpotifyAppRemote.getPlayerApi().skipPrevious();
+        }
     }
 
     public static void stopMusic(){
         if (mSpotifyAppRemote != null) {
             mSpotifyAppRemote.getPlayerApi().pause();
-            //SpotifyAppRemote.disconnect(mSpotifyAppRemote);
         }
     }
 
-    private static final String CLIENT_ID = "8cd0b669498e49d8b1915d6da500e789";
-    private static final String REDIRECT_URI = "https://localhost:8080";
-    public static SpotifyAppRemote mSpotifyAppRemote = null;
+    public static void playMusic(){
+        if (mSpotifyAppRemote != null) {
+            mSpotifyAppRemote.getPlayerApi().resume();
+        }
+    }
+
+    public static void nextMusic(){
+        if (mSpotifyAppRemote != null) {
+            mSpotifyAppRemote.getPlayerApi().skipNext();
+        }
+    }
 
 
     private static final String ARG_PARAM1 = "param1";
@@ -74,10 +168,30 @@ public class MusicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_music, container, false);
 
-        Button stopButton = view.findViewById(R.id.stopButton);
-        stopButton.setOnClickListener(lambda->{
-            stopMusic();
+        findAllTextViews(view);
+        findAllImageViews(view);
+
+        previousSong.setOnClickListener(lambda->{
+            previousMusic();
         });
+
+        playStopSong.setOnClickListener(lambda->{
+            if (gonna_play){
+                playMusic();
+                gonna_play = false;
+                playStopSong.setImageResource(R.drawable.ic_pause);
+            } else{
+                stopMusic();
+                gonna_play = true;
+                playStopSong.setImageResource(R.drawable.ic_play_arrow);
+            }
+        });
+
+        nextSong.setOnClickListener(lambda->{
+            nextMusic();
+        });
+
+
 
         ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
@@ -91,8 +205,6 @@ public class MusicFragment extends Fragment {
                     @Override
                     public void onConnected(SpotifyAppRemote spotifyAppRemote) {
                         mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
-
                         connected();
                     }
 
